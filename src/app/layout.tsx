9b5c6f7d86ea93e98bd305c4c82ad9f925bafb5b
jdidi94 <jdididaoud1994@gmail.com>
2025-02-27 21:29:22 +0000

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { TranslationProvider } from "@/context/useTranslation";
 export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 export const lora = Lora({ subsets: ["latin"], weight: ["400", "700"] });
 import { keywords } from "./keywords";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Create a separate JSON-LD script for location data
 export const locationSchema = {
@@ -117,24 +116,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-X00K9YR483"
-        ></script>
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-X00K9YR483');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W7Q9WZCR');
             `,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
       <body className="min-h-screen flex flex-col">
-        <SpeedInsights />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W7Q9WZCR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ThemeProvider>
           <TranslationProvider>
             <main className="flex-grow">{children}</main>
